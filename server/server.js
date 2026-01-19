d7a7334cae5cc358d0150ce653973c422aab9bb9
rahul7697762 <sainirahul5087@gmail.com>
2026-01-19 09:53:46 +0530

    Fix wildcard route for Express 5 compatibility

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // Handle React routing, return all requests to React app
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
